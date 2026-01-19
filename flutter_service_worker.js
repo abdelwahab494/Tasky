d1a80862f3b867da4beb836f3b959af0e8768d1e
Abdelwahab <aaaaaaaaaab743@gmail.com>
2026-01-19 14:29:36 +0200

    Optemizing Phone Layout

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,14 +26,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "f447ada9ea4285d49ff9285eb38b87a2
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "18c42bebdbbcb08cf27e941bb9e3cf07",
+"flutter_bootstrap.js": "3f241c43d4bb40bd0942a98203fb1c96",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "e8e378f7d25f1538ee413e678f879e5f",
 "/": "e8e378f7d25f1538ee413e678f879e5f",
-"main.dart.js": "e884ac81005fb5ecfb7082c68cf0a958",
+"main.dart.js": "1e030436d13dafacc682dd618066cf54",
 "manifest.json": "70e23aa251c7f3b57a912793e5c76607",
 "version.json": "7ac62b68349086ca2d19da8832ae946c"};
 // The application shell files that are downloaded before a service worker can
