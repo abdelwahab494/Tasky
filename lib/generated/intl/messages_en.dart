421295fac46898e2117d8897171dee82d52a2fb3
Abdelwahab <aaaaaaaaaab743@gmail.com>
2026-04-15 06:23:08 +0200

    Stable Version

--- lib/generated/intl/messages_en.dart
+++ lib/generated/intl/messages_en.dart
@@ -22,94 +22,83 @@ class MessageLookup extends MessageLookupByLibrary {
 
   final messages = _notInlinedMessages(_notInlinedMessages);
   static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
-    "addNewNote": MessageLookupByLibrary.simpleMessage("Add New Note"),
-    "addNewTask": MessageLookupByLibrary.simpleMessage("Add New Task"),
-    "addTask": MessageLookupByLibrary.simpleMessage("Add Task"),
-    "addyourfirstone": MessageLookupByLibrary.simpleMessage(
-      "Add your first one",
-    ),
-    "alltaskswillbedeletedpermanently": MessageLookupByLibrary.simpleMessage(
-      "All tasks will be deleted permanently.",
-    ),
-    "completed": MessageLookupByLibrary.simpleMessage("Completed"),
-    "completedTasks": MessageLookupByLibrary.simpleMessage("Completed Tasks"),
-    "darkMode": MessageLookupByLibrary.simpleMessage("Dark Mode"),
-    "delete": MessageLookupByLibrary.simpleMessage("Delete"),
-    "deleteAllTasks": MessageLookupByLibrary.simpleMessage("Delete All Tasks"),
-    "deleteNotes": MessageLookupByLibrary.simpleMessage("Delete Notes"),
-    "description": MessageLookupByLibrary.simpleMessage("Description"),
-    "egAbdelwahabMo": MessageLookupByLibrary.simpleMessage(
-      "e.g. Abdelwahab Mo",
-    ),
-    "egOnetaskatatimeOnestepcloser": MessageLookupByLibrary.simpleMessage(
-      "e.g. One task at a time. One step closer.",
-    ),
-    "hello": MessageLookupByLibrary.simpleMessage("Hello,"),
-    "highPriority": MessageLookupByLibrary.simpleMessage("High Priority"),
-    "highPriorityTasks": MessageLookupByLibrary.simpleMessage(
-      "High Priority Tasks",
-    ),
-    "home": MessageLookupByLibrary.simpleMessage("Home"),
-    "letsGetStarted": MessageLookupByLibrary.simpleMessage(
-      "Let\'s Get Started",
-    ),
-    "logOut": MessageLookupByLibrary.simpleMessage("Log Out"),
-    "motivationQuote": MessageLookupByLibrary.simpleMessage("Motivation Quote"),
-    "myTasks": MessageLookupByLibrary.simpleMessage("My Tasks"),
-    "noCompletedTasks": MessageLookupByLibrary.simpleMessage(
-      "No Completed Tasks",
-    ),
-    "noHighPriorityTasks": MessageLookupByLibrary.simpleMessage(
-      "No High Priority Tasks",
-    ),
-    "noNotesYet": MessageLookupByLibrary.simpleMessage("No Notes Yet"),
-    "noTasksToDo": MessageLookupByLibrary.simpleMessage("No Tasks To Do"),
-    "noTasksYet": MessageLookupByLibrary.simpleMessage("No Tasks Yet"),
-    "notes": MessageLookupByLibrary.simpleMessage("Notes"),
-    "notesomethingdown": MessageLookupByLibrary.simpleMessage(
-      "Note something down ..",
-    ),
-    "onetaskatatimeOnestepcloser": MessageLookupByLibrary.simpleMessage(
-      "One task at a time. One step closer.",
-    ),
-    "pleaseEnterTheTaskName": MessageLookupByLibrary.simpleMessage(
-      "Please Enter The Task Name.",
-    ),
-    "pleaseEnterYourName": MessageLookupByLibrary.simpleMessage(
-      "Please Enter Your Name.",
-    ),
-    "profile": MessageLookupByLibrary.simpleMessage("Profile"),
-    "profileInfo": MessageLookupByLibrary.simpleMessage("Profile Info"),
-    "relaxfornow": MessageLookupByLibrary.simpleMessage("Relax for now"),
-    "saveChanges": MessageLookupByLibrary.simpleMessage("Save Changes"),
-    "search": MessageLookupByLibrary.simpleMessage("Search"),
-    "sortDown": MessageLookupByLibrary.simpleMessage("Sort Down"),
-    "sortUp": MessageLookupByLibrary.simpleMessage("Sort Up"),
-    "startyourfirstone": MessageLookupByLibrary.simpleMessage(
-      "Start your first one",
-    ),
-    "takesomerestfornow": MessageLookupByLibrary.simpleMessage(
-      "Take some rest for now",
-    ),
-    "taskName": MessageLookupByLibrary.simpleMessage("taskName"),
-    "tasks": MessageLookupByLibrary.simpleMessage("tasks"),
-    "tasky": MessageLookupByLibrary.simpleMessage("Tasky"),
-    "theselectednoteswillbepermanentlydeleted":
-        MessageLookupByLibrary.simpleMessage(
-          "The selected notes will be permanently deleted.",
-        ),
-    "timetomakeprogress": MessageLookupByLibrary.simpleMessage(
-      "Time to make progress",
-    ),
-    "title": MessageLookupByLibrary.simpleMessage("Title"),
-    "toDo": MessageLookupByLibrary.simpleMessage("To Do"),
-    "toDoTasks": MessageLookupByLibrary.simpleMessage("To Do Tasks"),
-    "userDetails": MessageLookupByLibrary.simpleMessage("User Details"),
-    "userName": MessageLookupByLibrary.simpleMessage("User Name"),
-    "welcomeToTasky": MessageLookupByLibrary.simpleMessage("Welcome To Tasky"),
-    "yourName": MessageLookupByLibrary.simpleMessage("Your Name"),
-    "yourProductivityJourneyStartsHere": MessageLookupByLibrary.simpleMessage(
-      "Your productivity journey starts here.",
-    ),
-  };
+        "addNewNote": MessageLookupByLibrary.simpleMessage("Add New Note"),
+        "addNewTask": MessageLookupByLibrary.simpleMessage("Add New Task"),
+        "addTask": MessageLookupByLibrary.simpleMessage("Add Task"),
+        "addyourfirstone":
+            MessageLookupByLibrary.simpleMessage("Add your first one"),
+        "alltaskswillbedeletedpermanently":
+            MessageLookupByLibrary.simpleMessage(
+                "All tasks will be deleted permanently."),
+        "completed": MessageLookupByLibrary.simpleMessage("Completed"),
+        "completedTasks":
+            MessageLookupByLibrary.simpleMessage("Completed Tasks"),
+        "darkMode": MessageLookupByLibrary.simpleMessage("Dark Mode"),
+        "delete": MessageLookupByLibrary.simpleMessage("Delete"),
+        "deleteAllTasks":
+            MessageLookupByLibrary.simpleMessage("Delete All Tasks"),
+        "deleteNotes": MessageLookupByLibrary.simpleMessage("Delete Notes"),
+        "description": MessageLookupByLibrary.simpleMessage("Description"),
+        "egAbdelwahabMo":
+            MessageLookupByLibrary.simpleMessage("e.g. Abdelwahab Mo"),
+        "egOnetaskatatimeOnestepcloser": MessageLookupByLibrary.simpleMessage(
+            "e.g. One task at a time. One step closer."),
+        "hello": MessageLookupByLibrary.simpleMessage("Hello,"),
+        "highPriority": MessageLookupByLibrary.simpleMessage("High Priority"),
+        "highPriorityTasks":
+            MessageLookupByLibrary.simpleMessage("High Priority Tasks"),
+        "home": MessageLookupByLibrary.simpleMessage("Home"),
+        "letsGetStarted":
+            MessageLookupByLibrary.simpleMessage("Let\'s Get Started"),
+        "logOut": MessageLookupByLibrary.simpleMessage("Log Out"),
+        "motivationQuote":
+            MessageLookupByLibrary.simpleMessage("Motivation Quote"),
+        "myTasks": MessageLookupByLibrary.simpleMessage("My Tasks"),
+        "noCompletedTasks":
+            MessageLookupByLibrary.simpleMessage("No Completed Tasks"),
+        "noHighPriorityTasks":
+            MessageLookupByLibrary.simpleMessage("No High Priority Tasks"),
+        "noNotesYet": MessageLookupByLibrary.simpleMessage("No Notes Yet"),
+        "noTasksToDo": MessageLookupByLibrary.simpleMessage("No Tasks To Do"),
+        "noTasksYet": MessageLookupByLibrary.simpleMessage("No Tasks Yet"),
+        "notes": MessageLookupByLibrary.simpleMessage("Notes"),
+        "notesomethingdown":
+            MessageLookupByLibrary.simpleMessage("Note something down .."),
+        "onetaskatatimeOnestepcloser": MessageLookupByLibrary.simpleMessage(
+            "One task at a time. One step closer."),
+        "pleaseEnterTheTaskName":
+            MessageLookupByLibrary.simpleMessage("Please Enter The Task Name."),
+        "pleaseEnterYourName":
+            MessageLookupByLibrary.simpleMessage("Please Enter Your Name."),
+        "profile": MessageLookupByLibrary.simpleMessage("Profile"),
+        "profileInfo": MessageLookupByLibrary.simpleMessage("Profile Info"),
+        "relaxfornow": MessageLookupByLibrary.simpleMessage("Relax for now"),
+        "saveChanges": MessageLookupByLibrary.simpleMessage("Save Changes"),
+        "search": MessageLookupByLibrary.simpleMessage("Search"),
+        "sortDown": MessageLookupByLibrary.simpleMessage("Sort Down"),
+        "sortUp": MessageLookupByLibrary.simpleMessage("Sort Up"),
+        "startyourfirstone":
+            MessageLookupByLibrary.simpleMessage("Start your first one"),
+        "takesomerestfornow":
+            MessageLookupByLibrary.simpleMessage("Take some rest for now"),
+        "taskName": MessageLookupByLibrary.simpleMessage("taskName"),
+        "tasks": MessageLookupByLibrary.simpleMessage("tasks"),
+        "tasky": MessageLookupByLibrary.simpleMessage("Tasky"),
+        "theselectednoteswillbepermanentlydeleted":
+            MessageLookupByLibrary.simpleMessage(
+                "The selected notes will be permanently deleted."),
+        "timetomakeprogress":
+            MessageLookupByLibrary.simpleMessage("Time to make progress"),
+        "title": MessageLookupByLibrary.simpleMessage("Title"),
+        "toDo": MessageLookupByLibrary.simpleMessage("To Do"),
+        "toDoTasks": MessageLookupByLibrary.simpleMessage("To Do Tasks"),
+        "userDetails": MessageLookupByLibrary.simpleMessage("User Details"),
+        "userName": MessageLookupByLibrary.simpleMessage("User Name"),
+        "welcomeToTasky":
+            MessageLookupByLibrary.simpleMessage("Welcome To Tasky"),
+        "yourName": MessageLookupByLibrary.simpleMessage("Your Name"),
+        "yourProductivityJourneyStartsHere":
+            MessageLookupByLibrary.simpleMessage(
+                "Your productivity journey starts here.")
+      };
 }
